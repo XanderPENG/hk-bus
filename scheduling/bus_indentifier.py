066d2863c33d5eab539ed3b2e7d863ddbf56e292
XanderPENG <pzh980523@outlook.com>
2024-10-21 01:23:09 +0200

    update backward search - v1

--- scheduling/bus_indentifier.py
+++ scheduling/bus_indentifier.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+
+
 from treelib import Node, Tree
+from typing_extensions import deprecated
+
 
 class Eta:
     def __init__(self, id, co, line_id, dir, service_type, seq, dest, eta_seq, eta, rmk, scrapped_time):
@@ -27,14 +31,31 @@ class Eta:
                 and abs(self.eta-other.eta).total_seconds() / 60 <= time_threshold
                 )
 
-    def backward_search_feasible_etas(self, others: list, num=5):
+    def find_closet_earlier_etas(self, others: list, num=1):
         """
-        Search the feasible etas in the previous stops
+        Search n closet etas in the previous stops
         """
         feasible_etas = filter(lambda x: x.eta < self.eta, others)
         feasible_etas = sorted(feasible_etas, key=lambda x: x.eta, reverse=True)
         return feasible_etas[:num]
-        
+
+
+
+class BusRoute:
+    def __init__(self, co, line_id, dest,):
+        self.co = co
+        self.line_id = line_id
+        self.dest = dest
+
+        self.stops = []
+        self.etas = []
+        self.eta_idx = []
+
+    def complete_route_by_avg_etas(self, avg_etas:list):
+        """
+        Complete the route based on the average etas
+        """
+        pass
 
 class BusLineEta:
     def __init__(self, co, line_id, dir, service_type, dest, rmk, etas):
@@ -49,6 +70,7 @@ class BusLineEta:
         # dynamic
         self.etas = etas
         self.eta_groups = self.__initialize_eta_group__()
+        self.routes = []  # List of identified BusRoute
         ## Create a tree with the root node
         self.tree = Tree()
         self.tree.create_node(identifier=self.co+'_'+self.line_id)
@@ -57,6 +79,7 @@ class BusLineEta:
     def __initialize_eta_group__(self):
         """
         Split the etas into groups based on their stop_seq and scrapped_time
+        TODO: convert to a dictionary: {T: {seq: [etas]}}
         """
         eta_groups = {}
         eta_scrapped_times = set([eta.scrapped_time for eta in self.etas])
@@ -72,6 +95,30 @@ class BusLineEta:
 
         return eta_groups
 
+    def backward_search_feasible_routes(self):
+        """
+        Search all the feasible routes of this bus line:
+            1. Find the feasible etas in the previous stop within this scraped time;
+                a. if there is not any feasible eta, then try to find the feasible etas in the previous stop within the previous scraped time (till get a feasible eta)
+                b. if there are several possible etas, try to assess the feasibility of each eta (e.g., based on the mean_gap, or closet gap)
+            2. Create the bus route instance
+            3. Here, we can create a list to store and update the avg_gap of each stop pair,
+                which can be used to judge the feasibility of the etas.
+
+        feasibility assessment:
+        General: compare the 3 etas in Seq, T with the 3 etas in Seq-1, T
+            a. filter the etas greater than any of the 3 etas in Seq-1, T -> find the least variance option (or, find the least one in the previous group for the least one in the current group and then remove this option)
+            b. keep the BusRoute on waiting (or step to T-1 for searching) if the feasible etas are not found
+            c. remove selected etas from the eta_groups
+
+
+        :return:
+        """
+        pass
+
+
+
+    @deprecated
     def clean_etas(self, time_threshold: float=5):
         """
         Identify eta records with small-time difference (within the threshold; default 2 minutes),
@@ -127,6 +174,7 @@ class BusLineEta:
 
         return new_group
 
+    @deprecated
     def find_possible_consecutive_eta_pairs(self, num=5):
         """
         Find the possible consecutive eta pairs, and build the tree
@@ -134,7 +182,9 @@ class BusLineEta:
         num_seqs = len(self.eta_groups)
         _seq = num_seqs  # Since the seq starts from 1, ...
 
-        while _seq > 1:
+        all_candidate_eta_lists = []
+        all_candidate_eta_time_gaps = []
+        while _seq > 1:  # Backward search
             current_group = self.eta_groups.get(_seq)
             previous_group = self.eta_groups.get(_seq-1)
 
@@ -158,21 +208,23 @@ class BusLineEta:
                     g.append(time_gap)
                 candidate_eta_time_gaps.append(time_gap)
 
+            all_candidate_eta_lists.append(candidate_eta_lists)
+            all_candidate_eta_time_gaps.append(candidate_eta_time_gaps)
             # Find the combination with the smallest standard deviation of the time gap (Optimization)
-
-
             _seq -= 1
+
+        return all_candidate_eta_lists, all_candidate_eta_time_gaps
        
-        ''' 
-        Clean the tree: remove the nodes that do not have children
-        '''
-        is_feasible = False
-        while not is_feasible:
-            is_feasible = True
-            for node in self.tree.all_nodes():
-                if not self.tree.children(node.identifier):
-                    self.tree.remove_node(node.identifier)
-                    is_feasible = False
+        # '''
+        # Clean the tree: remove the nodes that do not have children
+        # '''
+        # is_feasible = False
+        # while not is_feasible:
+        #     is_feasible = True
+        #     for node in self.tree.all_nodes():
+        #         if not self.tree.children(node.identifier):
+        #             self.tree.remove_node(node.identifier)
+        #             is_feasible = False
         
         def __optimize_eta_pair_combination__():
             pass
